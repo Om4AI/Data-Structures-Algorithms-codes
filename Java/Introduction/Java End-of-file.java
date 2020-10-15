43774907c51fca0a91941aaaf4363a0732c9d794
Om Mule <70912643+Om-Infinite-Code@users.noreply.github.com>
2020-10-15 10:53:26 +0530

    Create Java End-of-file.java

---
+++ Java/Introduction/Java End-of-file.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int i=1;
+        while (s.hasNextLine()){
+            System.out.println(i+" "+s.nextLine());
+            i++;
+        }
+    }
+}
