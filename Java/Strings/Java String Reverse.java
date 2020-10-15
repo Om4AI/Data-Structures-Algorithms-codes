06237e11b24cd01c1e2ee322a394270aacad3b5f
Om Mule <70912643+Om-Infinite-Code@users.noreply.github.com>
2020-10-15 10:51:32 +0530

    Create Java String Reverse.java

---
+++ Java/Strings/Java String Reverse.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.math.*;
+
+
+public class Test_1{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String s1 = s.next();
+        int i,j;
+        int f=0;
+        int n1=s1.length();
+        String s2=s1.toLowerCase();
+        for (i=0;i<n1/2;i++) {
+            if (s2.charAt(i)!=s2.charAt(n1-1-i)) {
+                f=1;
+                break;
+            }
+            else {
+                continue;
+            }
+        }
+        System.out.print(f==0?"Yes":"No");
+    }
+}
