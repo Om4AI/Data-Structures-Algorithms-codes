d78a53826ade485573895f090a53a8530f082f0f
Om Mule <dgr8om@gmail.com>
2021-05-21 00:33:09 +0530

    Easy Java code

---
+++ LeetCode/Minimum Distance to the Target Element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int getMinDistance(int[] nums, int target, int start) {
+        int min = Integer.MAX_VALUE;
+        for (int i=0; i<nums.length; i++){
+            if (nums[i] == target){
+                if (Math.abs(i-start) < min){
+                    min = Math.abs(i-start);
+                }
+            }
+        }
+        return min;
+    }
+}
