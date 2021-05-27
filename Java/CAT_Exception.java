e9712bb65d33ac5e83a5eb79958d155a5fcd0785
Om Mule <dgr8om@gmail.com>
2021-05-27 11:40:19 +0530

    Java User Defined Exception

---
+++ Java/CAT_Exception.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+class InvalidCATException extends Exception{
+    InvalidCATException(String s){
+        super(s);
+    }
+}
+
+class InvalidQuizException extends Exception{
+    InvalidQuizException(String s){
+        super(s);
+    }
+}
+
+public class CAT_Exception{
+    static void validate_CAT(int c)throws InvalidCATException{
+        if (c<0 || c>100){throw new InvalidCATException("Invalid CAT marks!");}
+        else{System.out.println("CAT marks entered");}
+    }
+
+    static void validate_Quiz(int c)throws InvalidQuizException{
+        if (c<0 || c>10){throw new InvalidQuizException("Invalid Quiz marks!");}
+        else{System.out.println("Quiz marks entered");}
+    }
+
+
+    public static void main(String[] args){
+        System.out.println("\nName: Om Mule\nReg No: 19BCE0439");
+        int cat, quiz;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter CAT marks: ");
+        cat = s.nextInt();
+        System.out.println("Enter Quiz marks: ");
+        quiz = s.nextInt();
+        System.out.println("\n");
+
+        // CAT Marks
+        try{
+            validate_CAT(cat);
+        }catch(Exception e){System.out.println(e);}
+
+        // Quiz Marks
+        try{
+            validate_Quiz(quiz);
+        }catch(Exception e){System.out.println(e);}
+
+        finally{System.out.println("Marks Checked");}
+    }
+}
