f1b930b2d48651f9a675d2d6efde05d4be55aeab
Om Mule <dgr8om@gmail.com>
2021-07-16 11:06:19 +0530

    Java

---
+++ LeetCode/Number of Good Pairs.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int c = 0;
+        for (int i=0; i<nums.length-1; i++){
+            for (int j=i+1; j<nums.length; j++){
+                if(nums[i]==nums[j]){c+=1;}
+            }
+        }
+        return c;
+    }
+}
