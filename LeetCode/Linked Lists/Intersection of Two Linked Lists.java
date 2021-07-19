ce46ef2050d5b99048c5dec34e0a32f643fe126e
Om Mule <dgr8om@gmail.com>
2021-07-19 15:36:13 +0530

    Linked List: Java

---
+++ LeetCode/Linked Lists/Intersection of Two Linked Lists.java
@@ -0,0 +1,37 @@
+// Linked List: LeetCode Java code
+
+public class Solution {
+    
+    public static int len(ListNode a){
+        int len = 1;
+        while (a.next != null){
+            a = a.next;
+            len++;
+        }
+        return len;
+    }
+    
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//         Find the length of Lists
+        int lena = len(headA), lenb = len(headB);
+//         Make the starts same
+        if (lena>lenb){
+            while (lena>lenb){
+                headA = headA.next;
+                lena--;
+            }
+        }
+        else{
+            while (lenb>lena){
+                headB = headB.next;
+                lenb--;
+            }
+        }
+//         Find intersection
+        while (headA != headB){
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return headA;
+    }
+}
