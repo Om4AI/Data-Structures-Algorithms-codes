c326a737ec9816adeb5706606cc4526e54edd693
Om Mule <dgr8om@gmail.com>
2021-07-29 01:58:08 +0530

    Tree: Java

---
+++ LeetCode/Binary Tree Inorder Traversal.java
@@ -0,0 +1,11 @@
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root!=null){
+            list.addAll(inorderTraversal(root.left));
+            list.add(root.val);
+            list.addAll(inorderTraversal(root.right));
+        }
+        return list;
+    }
+}
