d86fed49e5d0e08d0a6bc3793f0dc7616d929ddc
Om Mule <dgr8om@gmail.com>
2021-08-01 00:04:33 +0530

    Java Code

--- LeetCode/Sort an Array.java
+++ LeetCode/Sort an Array.java
@@ -36,3 +36,68 @@ class Solution {
         return nums;
     }
 }
+
+
+// ------------------------------------------------------------
+
+
+// Merge Sort (Accepted) - 11ms
+class Solution {
+    
+    static void merge(int[] arr, int l, int m, int r){
+        int n1 = m-l+1;
+        int n2 = r-m;
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+        
+//         Create Arrays
+        for (int i=0; i<n1; ++i){L[i] = arr[l+i];}
+        for (int j=0; j<n2; ++j){R[j] = arr[m+1+j];}
+        
+        int i=0, j=0, k=l;
+        while (i<n1 && j<n2){
+            if (L[i] <= R[j]){
+                arr[k] = L[i];
+                i++;
+            }
+            else{
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        
+//         Remaining elements in subarrays
+        while (i<n1){
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        
+        while (j<n2){
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+    
+    static void msort(int[] arr, int l,int r){
+        if (l<r){
+            int m = l + (r-l)/2;
+        
+//         Divide & Sort subarrays
+            msort(arr, l, m);
+            msort(arr, m+1, r);
+            
+            merge(arr, l, m, r);
+        }
+    }
+    
+    
+    
+    
+    public int[] sortArray(int[] arr) {
+        msort(arr, 0, arr.length-1);
+        return arr;
+    }
+}
