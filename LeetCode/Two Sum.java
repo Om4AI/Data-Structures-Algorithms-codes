ad944f951593d05003c0d0a84334706916aac053
Om Mule <dgr8om@gmail.com>
2021-08-02 14:32:05 +0530

    Java Code

--- LeetCode/Two Sum.java
+++ LeetCode/Two Sum.java
@@ -1,3 +1,5 @@
+// Brute Force Approach
+
 class Solution {
     public int[] twoSum(int[] arr, int target) {
         
@@ -11,6 +13,23 @@ class Solution {
                     break;
                 }
             }
+        }
+        return res;
+    }
+}
+
+// Better time complexity (HashMap)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int res[] = new int[2];
+        HashMap<Integer, Integer> map = new HashMap();
+        for (int i=0; i<nums.length; i++){
+            if (map.containsKey(target- nums[i])){
+                res[0] = map.get(target- nums[i]);
+                res[1] = i;
+                return res;
+            }
+            map.put(nums[i], i);
         }
         return res;
     }
