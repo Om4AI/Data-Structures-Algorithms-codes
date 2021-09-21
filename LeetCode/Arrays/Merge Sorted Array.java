83d427b25c562d3594b7670185c0f86c6aba37b8
Om Mule <dgr8om@gmail.com>
2021-09-21 18:30:05 +0530

    Arrays Java

--- LeetCode/Arrays/Merge Sorted Array.java
+++ LeetCode/Arrays/Merge Sorted Array.java
@@ -1,3 +1,22 @@
+// Brute Force Approach
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        List<Integer> l = new ArrayList<>();
+        for (int i=0; i<m; i++){
+            l.add(nums1[i]);
+        }
+        for (int n1: nums2){
+            l.add(n1);
+        }
+        Collections.sort(l);
+        for (int i=0;i<m+n; i++){
+            nums1[i] = l.get(i);
+        }
+    }
+}
+
+
+
 // Merge Sorted Array: Java Code (0ms)
 
 class Solution {
