e08a480e2e337ac3f3de50020df231ade6bf3ab5
Om Mule <dgr8om@gmail.com>
2021-09-25 19:55:01 +0530

    Strings Java

---
+++ LeetCode/Strings/Valid Anagram.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length()!=t.length()) return false;
+        char c1[] = s.toCharArray();
+        char c2[] = t.toCharArray();
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+        for (int i=0; i<c1.length; i++){
+            if (c1[i]!=c2[i]) return false;
+        }
+        return true;
+    }
+}
