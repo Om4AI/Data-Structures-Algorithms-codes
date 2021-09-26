17e25545447ece7f1c4d57872cb2da4c4b4edc07
Om Mule <dgr8om@gmail.com>
2021-09-26 17:46:44 +0530

    Java Code

--- LeetCode/Algorithms/Binary Search.java
+++ LeetCode/Algorithms/Binary Search.java
@@ -1,13 +1,14 @@
 class Solution {
     public int search(int[] arr, int target) {
-        int start = 0, end = arr.length-1;
+        int start = 0, end = arr.length;
         int mid  = (start+end)/2;
-        while (start<=end){
-            mid  = (start+end)/2;
+        while (start<end){
+            mid  = start + (end-start)/2;
             if (arr[mid]==target) return mid;
             else if (target>arr[mid]) start = mid+1;
             else end = mid-1;
         }
+        if (end<arr.length && start==end && arr[start]==target) return start; 
         return -1;
     }
 }
