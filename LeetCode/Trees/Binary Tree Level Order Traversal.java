c9ab2af09d1e293942a23cf2acfcac8c4614f090
Om Mule <dgr8om@gmail.com>
2021-09-30 09:20:53 +0530

    J Code

--- LeetCode/Trees/Binary Tree Level Order Traversal.java
+++ LeetCode/Trees/Binary Tree Level Order Traversal.java
@@ -1,28 +1,25 @@
 // Code by @Om
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> res = new ArrayList();
-        if (root==null) return res;
-
-        Queue<TreeNode> queue = new LinkedList();
-        queue.add(root);
-
-        // Queue Operations
-        while (!queue.isEmpty()){
-            int size = queue.size();
-            List<Integer> l = new ArrayList();
-            for(int i=0; i<size; i++){
-                TreeNode node = queue.remove();
+        List<List<Integer>> res=new ArrayList<>();
+//         Empty Case
+        if (root ==null) return res;
+//         General cases
+        Queue<TreeNode> q = new LinkedList();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            List<Integer> l = new ArrayList<>();
+            for (int i=0; i<size; i++){
+                TreeNode node = q.poll();
                 l.add(node.val);
-                if (node.left!=null){
-                    queue.add(node.left);
-                }
-                if (node.right!=null){
-                    queue.add(node.right);
-                }
+                if (node.left!=null) q.add(node.left);
+                if (node.right!=null) q.add(node.right);
             }
             res.add(l);
         }
-        return res;
+        return res;  
     }
 }
