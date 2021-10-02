f16d338deac17a414a961d64bc6efa592c3d7f4b
Om Mule <dgr8om@gmail.com>
2021-10-02 18:33:11 +0530

    BST Java

--- LeetCode/Trees/Search in a Binary Search Tree.java
+++ LeetCode/Trees/Search in a Binary Search Tree.java
@@ -1,14 +1,14 @@
 // Code by @Om
 class Solution {
+    TreeNode res;
     public TreeNode searchBST(TreeNode root, int val) {
+//         Blank tree case
         if (root==null) return null;
-        else if (root.val == val) return root;
-        else if (val>root.val) {
-            root = searchBST(root.right, val);
-        }
-        else if (val < root.val) {
-            root = searchBST(root.left, val);
-        }
-        return root;
+        
+        if (val==root.val) return root;
+        else if (val<root.val) return searchBST(root.left, val);
+        else if (val>root.val) return searchBST(root.right, val);
+        
+        return null;
     }
 }
