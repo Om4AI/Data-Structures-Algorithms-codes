71a64f12b1e404f7c0e089d31f38bf84466432af
Om Mule <dgr8om@gmail.com>
2021-11-10 19:54:48 +0530

    J

---
+++ InBit/FizzBuzz.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public ArrayList<String> fizzBuzz(int n) {
+        ArrayList<String> res = new ArrayList<>();
+        for (int i=1; i<=n; i++){
+            if(i%3==0 && i%5==0) res.add("FizzBuzz");
+            else if(i%3==0) res.add("Fizz");
+            else if(i%5==0) res.add("Buzz");
+            else res.add(String.valueOf(i));
+        }
+        return res;
+    }
+}
