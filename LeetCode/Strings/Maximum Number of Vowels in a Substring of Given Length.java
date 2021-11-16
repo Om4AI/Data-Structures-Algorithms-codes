56daca50338b9a6fa97ab2efb997e36c8ac0db09
Om Mule <dgr8om@gmail.com>
2021-11-16 12:59:43 +0530

    STR J

---
+++ LeetCode/Strings/Maximum Number of Vowels in a Substring of Given Length.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+// SLIDING WINDOW
+class Solution {
+    public int maxVowels(String s, int k) {
+        StringBuffer sb = new StringBuffer();
+        int p=0, max=Integer.MIN_VALUE;
+        for (p=0; p<k; p++) sb.append(s.charAt(p));
+        int count = getVowels(sb.toString());
+
+        max = Math.max(max, count);
+
+        // MAIN LOOP 
+        while(p<s.length()){
+            if(isVowel(sb.charAt(0))) count--;
+            sb.deleteCharAt(0);
+            if(isVowel(s.charAt(p))) count++;
+            sb.append(s.charAt(p));
+            p++;
+            max = Math.max(max, count);
+        }
+        return max;
+    }
+
+    // Check if a character is a vowel or not
+    public boolean isVowel(char c){
+        if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u') return true;
+        return false;
+    }
+
+    // Count the number of vowels in the String
+    public int getVowels(String s){
+        int count = 0;
+        for (int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u') count++;
+        }
+        return count;
+    }
+}
