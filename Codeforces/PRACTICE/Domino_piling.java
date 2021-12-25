c4809cf9447877744f557fcad258cd336f342351
Om Mule <dgr8om@gmail.com>
2021-12-26 00:14:48 +0530

    50A

---
+++ Codeforces/PRACTICE/Domino_piling.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Domino_piling {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int m = s.nextInt();
+        int n = s.nextInt();
+        System.out.println((m*n)/2);
+    }
+}
