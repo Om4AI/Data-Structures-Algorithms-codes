e3fe9dcb149b5e9b314f9015e7c78abd0a9f75a8
Om Mule <dgr8om@gmail.com>
2022-01-30 22:15:12 +0530

    Min # of Insertions - DP J Leetcode

---
+++ Dynamic Programming (DP)/LCS/Minimum Insertion Steps to Make a String Palindrome.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minInsertions(String s){
+        StringBuffer sb = new StringBuffer(s);
+        String b = sb.reverse().toString();
+        int m = s.length();
+        int t[][] = new int[m+1][m+1];
+        
+//         Init
+        for(int i=0; i<m+1; i++){
+            for(int j=0; j<m+1; j++){
+                if(i==0 || j==0) t[i][j] = 0;
+            }
+        }
+        
+//         Conditions
+        for(int i=1; i<m+1; i++){
+            for(int j=1; j<m+1; j++){
+                if(s.charAt(i-1)==b.charAt(j-1)) t[i][j] = 1+t[i-1][j-1];
+                else{
+                    t[i][j] = Math.max(t[i-1][j], t[i][j-1]);
+                }
+            }
+        }
+//       Number of letters after Palindrome are those that don't have their palindrome replica; thus that many insertions are necessary
+        int lps = t[m][m];
+        return m-lps;
+    }
+}
