886e59bd06a7a29bd81defd0f9983c55fe4caadb
Om Mule <dgr8om@gmail.com>
2022-02-28 16:03:57 +0530

    Java

---
+++ LeetCode/Arrays/Can Make Arithmetic Progression From Sequence.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        if(arr.length==1 || arr.length==0) return true;
+        Arrays.sort(arr);
+        int diff = arr[1]-arr[0];
+        for(int i=1; i<arr.length-1; i++){
+            if (arr[i+1]-arr[i] != diff) return false;
+        }
+        return true;
+    }
+}
