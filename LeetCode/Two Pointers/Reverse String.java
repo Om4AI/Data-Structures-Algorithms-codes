96694052e035c8af0711e37babf53ff4c0274519
Om Mule <dgr8om@gmail.com>
2022-04-01 19:33:07 +0530

    2 PTR Java

--- LeetCode/Two Pointers/Reverse String.java
+++ LeetCode/Two Pointers/Reverse String.java
@@ -1,10 +1,11 @@
 class Solution {
     public void reverseString(char[] s) {
-        int i=0, j=s.length-1;
-        while(i<j){
-            char c=s[i];
-            s[i]=s[j];
-            s[j]=c;
+        // 2 pointers approach
+        int i =0, j = s.length-1;
+        while(i<=j){
+            char t = s[i];
+            s[i] = s[j];
+            s[j] = t;
             i++;
             j--;
         }
