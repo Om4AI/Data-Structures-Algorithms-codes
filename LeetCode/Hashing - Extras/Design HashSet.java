e48d3b243b33b50993d439f915292d9484724bca
Om Mule <dgr8om@gmail.com>
2022-04-21 14:27:27 +0530

    Hashing Java

---
+++ LeetCode/Hashing - Extras/Design HashSet.java
@@ -0,0 +1,26 @@
+class MyHashSet {
+    List<Integer> l;
+    public MyHashSet() {
+        l = new ArrayList<>();
+    }
+    
+    public void add(int key) {
+        if(!l.contains(key))l.add(key);
+    }
+    
+    public void remove(int key) {
+        if(l.contains(key)) l.remove(l.indexOf(key));
+    }
+    
+    public boolean contains(int key) {
+        return l.contains(key);
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
