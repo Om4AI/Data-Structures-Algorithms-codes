05990bbffaba6e3c1d240c64213f1dca8ad3701e
Om Mule <dgr8om@gmail.com>
2022-05-27 12:46:23 +0530

    BST Recovery Java
    https://leetcode.com/problems/recover-binary-search-tree/

--- LeetCode/Trees/Recover Binary Search Tree.java
+++ LeetCode/Trees/Recover Binary Search Tree.java
@@ -14,12 +14,14 @@ class Solution {
         modifyTree(root,a,b,-1,-1);
     }
     
+
     
+    // ------------------ModifyTree Function --------------------
     public static void modifyTree(TreeNode root, int v1, int v2, int fv1, int fv2){
-//         Base Case
+        // Base Case
         if(root==null) return;
         
-//         Modify the root node value
+        // Modify the root node value
         if(root.val==v1 && fv1==-1){
             root.val = v2;
             fv1 = 1;
@@ -28,18 +30,21 @@ class Solution {
             fv2 = 1;
         }
         
-//         Recursive call on subtrees
+        // Recursive call on subtrees
         modifyTree(root.left,v1,v2,fv1,fv2);
         modifyTree(root.right,v1,v2,fv1,fv2);
     }
     
+
+
+    // ------------------InOrderTraversal----------------
     public static List<Integer> inOrderTraversal(TreeNode root){
         List<Integer> res = new ArrayList<>();
         
-//         Base case
+        // Base case
         if(root==null) return res;
         
-//         Inorder traversal
+        // Inorder traversal
         res.addAll(inOrderTraversal(root.left));
         res.add(root.val);
         res.addAll(inOrderTraversal(root.right));
