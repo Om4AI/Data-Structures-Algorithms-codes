acdc81a6a7dece66767ada01aa4f5dc243cdc5a1
Om Mule <dgr8om@gmail.com>
2022-06-08 00:46:32 +0530

    Java

---
+++ Codechef/STARTERS 41/CBSPEED.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class CBSPEED{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt(), y = s.nextInt();
+
+        String res = (y>x)? "YES":"NO";
+        System.out.println(res);
+    }
+}
