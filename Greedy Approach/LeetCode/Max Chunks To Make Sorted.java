cb7236aa4e19912055eb44a44be057ec702193dc
Om Mule <dgr8om@gmail.com>
2022-06-16 01:07:31 +0530

    Greedy Approach & Max Heap - Java (Good Q)
    https://leetcode.com/problems/max-chunks-to-make-sorted/

--- Greedy Approach/LeetCode/Max Chunks To Make Sorted.java
+++ Greedy Approach/LeetCode/Max Chunks To Make Sorted.java
@@ -8,14 +8,13 @@ class Solution {
         // Count the maximum chunks that can be made
         int c = 0, start = -1, n = arr.length;
         for(int i=0; i<n; i++){
-            int curr = i;
             maxheap.add(arr[i]);
             
             // If (top=index & size=index-start)
             if(maxheap.peek()==i && maxheap.size()==(i-start)){
                 c++;
                 maxheap.clear();
-                start = curr;
+                start = i;
             }
         }
         return c;
