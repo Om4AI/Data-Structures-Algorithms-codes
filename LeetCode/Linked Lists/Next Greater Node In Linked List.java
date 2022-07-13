8e5157fd6f99e1b5f9d8de878c54098054120056
Om Mule <dgr8om@gmail.com>
2022-07-13 14:44:57 +0530

    LinkedList - Java
    https://leetcode.com/problems/next-greater-node-in-linked-list/

--- LeetCode/Linked Lists/Next Greater Node In Linked List.java
+++ LeetCode/Linked Lists/Next Greater Node In Linked List.java
@@ -9,6 +9,7 @@
  * }
  */
 import java.util.*;
+// ---------------------------------- Using 2 pointers O(n^2)-----------------------------------
 class Solution {
     public int[] nextLargerNodes(ListNode head){
 
@@ -38,6 +39,39 @@ class Solution {
 
             p = p.next;
             i++;
+        }
+        return res;
+    }
+}
+
+
+
+
+// ---------------------------------------- Using Stack --------------------------------------------
+class Solution {
+    public int[] nextLargerNodes(ListNode head){
+
+        // Find the length of the LinkedList
+        ListNode p = head;
+        int n = 0;
+        List<Integer> l = new ArrayList<>();
+        while(p!=null){
+            n++;
+            l.add(p.val);
+            p = p.next;  
+        }
+
+        int res[] = new int[n];
+        Stack<int[]> stk = new Stack<>();
+        for(int i=0; i<n; i++){
+            int num = l.get(i);
+            if(stk.isEmpty() || num<=stk.peek()[0]) stk.push(new int[]{num,i});
+            else if(num>stk.peek()[0]){
+                while(!stk.isEmpty() && num>stk.peek()[0]){
+                    res[stk.pop()[1]] = num;
+                }
+                stk.push(new int[]{num,i});
+            }
         }
         return res;
     }
