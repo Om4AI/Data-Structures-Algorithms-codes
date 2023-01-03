e75d6296ba18f6a1a03c65d482c1b1b90aa723e4
Om Mule <dgr8om@gmail.com>
2023-01-03 23:01:12 +0530

    Matrix & String - Java

---
+++ LeetCode/Matrix/Delete Columns to Make Sorted.java
@@ -0,0 +1,26 @@
+
+// Brute Force Approach
+class Solution {
+    public int minDeletionSize(String[] strs){
+        int m = strs.length, n = strs[0].length();
+        Character[][] mat = new Character[m][n];
+        int res = 0;
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                mat[i][j] = strs[i].charAt(j);
+            }
+        }
+
+        // Check each column 
+        for(int j=0; j<n; j++){
+            for(int i=0; i<m-1; i++){
+                if((mat[i][j] - mat[i+1][j])>0){
+                    res++;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+}
