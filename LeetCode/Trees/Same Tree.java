c1e8b5ea035b7914cdf45dd213cb3cde8daa5707
Om Mule <dgr8om@gmail.com>
2023-01-10 15:59:33 +0530

    Tree & Recursion - Java

--- LeetCode/Trees/Same Tree.java
+++ LeetCode/Trees/Same Tree.java
@@ -1,3 +1,21 @@
+// Solution 2: Better Solution - Shorter and Lesser time
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // Base case
+        if(p==null && q==null) return true;
+        else if((p==null && q!=null)||(p!=null && q==null)) return false;
+
+        // General case
+        if((p.val==q.val) && isSameTree(p.left,q.left) && isSameTree(p.right,q.right)) return true;
+        return false;
+    }
+}
+
+
+
+
+
+// Solution 1: First Solution 
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
 //         Recursive method 
