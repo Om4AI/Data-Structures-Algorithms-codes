40d04deeb10f0a833824125352973fad92708eeb
Om Mule <dgr8om@gmail.com>
2023-12-18 10:32:39 +0530

    Sorting

---
+++ LeetCode/Arrays/1913 - Maximum Product Difference.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Solution {
+    public int maxProductDifference(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        return ((nums[n-2]*nums[n-1]) - (nums[0]*nums[1]));
+    }
+}
