5050c933b9973a772400a99238ee97ec7692fb70
Om Mule <dgr8om@gmail.com>
2026-05-31 23:20:32 +0530

    Tabulation approach - Bottom Up approach
    Refactor climbStairs method to use memoization and improve base case handling.

--- Dynamic Programming (DP)/Leetcode/Climbing Stairs BottomUp.java
+++ Dynamic Programming (DP)/Leetcode/Climbing Stairs BottomUp.java
@@ -1,16 +1,15 @@
 class Solution {
     public int climbStairs(int n) {
-        int dp[] = new int[n+1];
-        
-        // Bottom Up Approach
-        for(int i=n-1; i>=0; i--){
-            // Base cases
-            if(i==n-1) dp[i] = 1;
-            else if(i==n-2) dp[i] = 2;
-            else{
-                dp[i] = dp[i+1]+dp[i+2];
-            }
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+
+        if (n<=1) return dp[n] = n;
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        return dp[0];
+        return dp[n];
     }
 }
