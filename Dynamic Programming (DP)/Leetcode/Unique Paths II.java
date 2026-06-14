9550f73445661e0d1e28bdb55aaee45a0bbe9feb
Om Mule <dgr8om@gmail.com>
2026-06-14 18:02:16 +0530

    Refactor uniquePathsWithObstacles using memoization

--- Dynamic Programming (DP)/Leetcode/Unique Paths II.java
+++ Dynamic Programming (DP)/Leetcode/Unique Paths II.java
@@ -1,32 +1,30 @@
+// DP - Memoization (Top down approach)
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid){
-        int m = obstacleGrid.length, n = obstacleGrid[0].length;
-
-        // Start point is blocked by obstacle
-        if(obstacleGrid[0][0]==1) return 0;
-        int t[][] = new int[m+1][n+1];
-        
-        // Initialization
-        for(int i=0; i<m+1; i++){
-            Arrays.fill(t[i], -1);
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        for (int i=0; i<m; i++){
+            Arrays.fill(dp[i], -1);
         }
-        int i=0, j=0;
-        return uniquePathsII(i,j,m,n,t,obstacleGrid);
+        if (obstacleGrid[m-1][n-1] == 1) return 0;
+        return fetchUniquePaths(m-1, n-1, obstacleGrid, dp);
     }
 
-    public static int uniquePathsII(int i,int j,int m,int n,int[][] t,int[][] obstacleGrid){
-        
-        // Base condition
-        if(i==m-1 && j==n-1) return 1;
+    int fetchUniquePaths(int i, int j, int[][] obstacleGrid, int[][] dp){
+        // Base case
+        if (i==0 && j==0) return dp[0][0] = 1;
+        if (i<0 || j<0) return 0;
 
-        // Check if memoization matrix has value
-        if(t[i][j]!=-1) return t[i][j];
+        // Memoization check    
+        if (dp[i][j] != -1) return dp[i][j];
 
-        // Actual logic 
-        int right =0, down=0;
-        if(i+1<=m-1 && obstacleGrid[i+1][j]==0) down = uniquePathsII(i+1,j,m,n,t,obstacleGrid);
-        if(j+1<=n-1 && obstacleGrid[i][j+1]==0) right = uniquePathsII(i,j+1,m,n,t,obstacleGrid);
+        int left = fetchUniquePaths(i, j-1, obstacleGrid, dp);
+        int up = fetchUniquePaths(i-1, j, obstacleGrid, dp);
 
-        return t[i][j] = right+down;
+        if (i!=0 && j!=0 && obstacleGrid[i-1][j]==1 && obstacleGrid[i][j-1]==1) return dp[i][j] = 0;
+        else if (i!=0 && obstacleGrid[i-1][j] == 1)return dp[i][j] = left;
+        else if (j!=0 && obstacleGrid[i][j-1] == 1) return dp[i][j] = up;
+        else return dp[i][j] = left + up;
     }
 }
