d6556694d235f3c63fcf0ca9ba6001c1cdebdb51
Om <ommule401@gmail.com>
2026-09-14 00:11:54 +0530

    Greedy basic example

--- LeetCode/Greedy/2410 - Maximum Matching of Players With Trainers.java
+++ LeetCode/Greedy/2410 - Maximum Matching of Players With Trainers.java
@@ -1,5 +1,17 @@
-package Leetcode.Greedy;
+// Sort the arrays to fulfill the training requirement
+class Solution {
+    public int matchPlayersAndTrainers(int[] players, int[] trainers){
+        Arrays.sort(trainers);
+        Arrays.sort(players);
 
-public class 2410 - Maximum Matching of Players With Trainers {
-
+        int tp=0, pp = 0, count = 0;
+        while (tp<trainers.length && pp<players.length){
+            if (trainers[tp]>=players[pp]){
+                count++;
+                tp++;
+                pp++;
+            }else if (trainers[tp]<players[pp]) tp++;
+        }
+        return count;
+    }
 }
